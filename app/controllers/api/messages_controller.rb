15ee80b7eb475aa9b38014ef768b1f1ec09ace94
Yigit Mersin <yigitmersin@gmail.com>
2022-04-27 21:31:48 +0300

    Create DB, controller, api endpoint

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,10 @@
+class Api::MessagesController < ApplicationController
+  def index
+    @message = Message.order('RANDOM()').first
+    if @message
+      render json: { success: true, message: 'Message(s) loaded.', data: { greeting: @message } }, status: :ok
+    else
+      render json: { success: false, errors: 'ERROR...Message not load!' }, status: :unprocessable_entity
+    end
+  end
+end
